745eca7db823525f5218a1ad0d191b7dab3efdc9
BingoJYB <7303639+BingoJYB@users.noreply.github.com>
2018-10-19 14:03:01 +0200

    Update find max distance.py

--- find max distance.py
+++ find max distance.py
@@ -1,14 +1,14 @@
 import math
 
-ls = [[2, 5], [0, 3], [9, 11], [4, 8]]
-sorted_ls = sorted(ls)
-max_dis = -math.inf
-for i in range(len(sorted_ls)-1):
-    if sorted_ls[i][1] >= sorted_ls[i+1][0]:
-        sorted_ls[i+1][0] = sorted_ls[i][0]
-    else:
-        dis = sorted_ls[i][1] - sorted_ls[i][0]
-        if dis > max_dis:
-            max_dis = dis
 
-print(max_dis)
+def solution(ls):
+    sorted_ls = sorted(ls)
+    max_dis = -math.inf
+    for i in range(len(sorted_ls) - 1):
+        if sorted_ls[i][1] >= sorted_ls[i + 1][0]:
+            sorted_ls[i + 1][0] = sorted_ls[i][0]
+        else:
+            dis = sorted_ls[i][1] - sorted_ls[i][0]
+            if dis > max_dis:
+                max_dis = dis
+    print(max_dis)
