e5deb11bfe4737431df86bbf506680e334e7389f
BingoJYB <7303639+BingoJYB@users.noreply.github.com>
2018-11-16 14:49:59 +0100

    Add find maximum level sum

---
+++ find maximum level sum in binary tree.py
@@ -0,0 +1,61 @@
+'''
+    Find maximum level sum in Binary Tree
+    Given a Binary Tree having positive and negative nodes, the task is to find maximum sum level in it.
+'''
+
+
+import math
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def get_level_sum(root):
+    if root is None:
+        return 0
+
+    queue = [root]
+    maximum = -math.inf
+
+    while len(queue) > 0:
+        temp = queue[:]
+        queue = []
+        sum = 0
+
+        while len(temp) > 0:
+            node = temp.pop(0)
+            sum += node.data
+
+            if node.left is not None:
+                queue.append(node.left)
+
+            if node.right is not None:
+                queue.append(node.right)
+
+        maximum = max(maximum, sum)
+
+    return maximum
+
+
+root = Node(1)
+node1 = Node(2)
+node2 = Node(3)
+node3 = Node(4)
+node4 = Node(5)
+node5 = Node(8)
+node6 = Node(6)
+node7 = Node(7)
+
+root.left = node1
+root.right = node2
+node1.left = node3
+node1.right = node4
+node2.right = node5
+node5.left = node6
+node5.right = node7
+
+print(get_level_sum(root))
