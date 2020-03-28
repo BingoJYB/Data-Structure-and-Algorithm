776a5d4b2b816960dd5e563b3dad5c2329a59392
JYB <7303639+BingoJYB@users.noreply.github.com>
2020-03-28 14:06:13 +0100

    repeated substring pattern

---
+++ 459.repeated-substring-pattern.java
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode id=459 lang=java
+ *
+ * [459] Repeated Substring Pattern
+ *
+ * https://leetcode.com/problems/repeated-substring-pattern/description/
+ *
+ * algorithms
+ * Easy (41.49%)
+ * Likes:    1218
+ * Dislikes: 135
+ * Total Accepted:    111.2K
+ * Total Submissions: 267.3K
+ * Testcase Example:  '"abab"'
+ *
+ * Given a non-empty string check if it can be constructed by taking a
+ * substring of it and appending multiple copies of the substring together. You
+ * may assume the given string consists of lowercase English letters only and
+ * its length will not exceed 10000.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "abab"
+ * Output: True
+ * Explanation: It's the substring "ab" twice.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "aba"
+ * Output: False
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "abcabcabcabc"
+ * Output: True
+ * Explanation: It's the substring "abc" four times. (And the substring
+ * "abcabc" twice.)
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int l = s.length();
+        for (int i = l / 2; i >= 1; i--) {
+            if (l % i == 0) {
+                int m = l / i;
+                String subS = s.substring(0, i);
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < m; j++) {
+                    sb.append(subS);
+                }
+                if (sb.toString().equals(s))
+                    return true;
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
