32c3180a8c3e6ed890c4c191d9afbe9eaa7a73dc
JYB <7303639+BingoJYB@users.noreply.github.com>
2020-04-01 23:53:29 +0200

    minstack

---
+++ 155.min-stack.java
@@ -0,0 +1,82 @@
+import java.util.Stack;
+
+/*
+ * @lc app=leetcode id=155 lang=java
+ *
+ * [155] Min Stack
+ *
+ * https://leetcode.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Easy (41.10%)
+ * Likes:    2721
+ * Dislikes: 279
+ * Total Accepted:    423.5K
+ * Total Submissions: 1M
+ * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ * Design a stack that supports push, pop, top, and retrieving the minimum
+ * element in constant time.
+ * 
+ * 
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
+ * 
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+class MinStack {
+    Stack<Integer> stack;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        
+    }
+    
+    public void push(int x) {
+        
+    }
+    
+    public void pop() {
+        
+    }
+    
+    public int top() {
+        
+    }
+    
+    public int getMin() {
+        
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+// @lc code=end
+
