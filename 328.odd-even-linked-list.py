7244885cedd59e6687901ebf6107bea550b99cc4
JYB <7303639+BingoJYB@users.noreply.github.com>
2021-09-24 19:52:46 +0200

    Odd Even Linked List

---
+++ 328.odd-even-linked-list.py
@@ -0,0 +1,81 @@
+#
+# @lc app=leetcode id=328 lang=python3
+#
+# [328] Odd Even Linked List
+#
+# https://leetcode.com/problems/odd-even-linked-list/description/
+#
+# algorithms
+# Medium (58.11%)
+# Likes:    3806
+# Dislikes: 354
+# Total Accepted:    427K
+# Total Submissions: 734.2K
+# Testcase Example:  '[1,2,3,4,5]'
+#
+# Given the head of a singly linked list, group all the nodes with odd indices
+# together followed by the nodes with even indices, and return the reordered
+# list.
+# 
+# The first node is considered odd, and the second node is even, and so on.
+# 
+# Note that the relative order inside both the even and odd groups should
+# remain as it was in the input.
+# 
+# You must solve the problem in O(1) extra space complexity and O(n) time
+# complexity.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: head = [1,2,3,4,5]
+# Output: [1,3,5,2,4]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: head = [2,1,3,5,6,4,7]
+# Output: [2,3,6,7,1,5,4]
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# n == number of nodes in the linked list
+# 0 <= n <= 10^4
+# -10^6 <= Node.val <= 10^6
+# 
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        odd_head, even_head = ListNode(), ListNode()
+        odd, even = odd_head, even_head
+        count = 1
+
+        while head:
+            if count % 2 == 1:
+                odd.next = ListNode(val=head.val)
+                odd = odd.next
+            else:
+                even.next = ListNode(val=head.val)
+                even = even.next
+
+            count += 1
+            head = head.next
+
+        odd.next = even_head.next
+
+        return odd_head.next
+# @lc code=end
+
