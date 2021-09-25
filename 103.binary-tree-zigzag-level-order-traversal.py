f0e9c24520b6d9c0e5d5ac64aaf0d4cf3f0e2d41
JYB <7303639+BingoJYB@users.noreply.github.com>
2021-09-25 20:11:29 +0200

    Binary Tree Zigzag Level Order Traversal

---
+++ 103.binary-tree-zigzag-level-order-traversal.py
@@ -0,0 +1,98 @@
+#
+# @lc app=leetcode id=103 lang=python3
+#
+# [103] Binary Tree Zigzag Level Order Traversal
+#
+# https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
+#
+# algorithms
+# Medium (51.70%)
+# Likes:    4287
+# Dislikes: 140
+# Total Accepted:    578.9K
+# Total Submissions: 1.1M
+# Testcase Example:  '[3,9,20,null,null,15,7]'
+#
+# Given the root of a binary tree, return the zigzag level order traversal of
+# its nodes' values. (i.e., from left to right, then right to left for the next
+# level and alternate between).
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: root = [3,9,20,null,null,15,7]
+# Output: [[3],[20,9],[15,7]]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: root = [1]
+# Output: [[1]]
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: root = []
+# Output: []
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# The number of nodes in the tree is in the range [0, 2000].
+# -100 <= Node.val <= 100
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        level = 1
+        q = []
+        res = []
+        
+        if root:
+            q.append(root)
+            temp = []
+            nodes = []
+
+            while q:
+                node = q.pop()
+                nodes.append(node.val)
+
+                if level % 2 == 1:
+                    if node.left:
+                        temp.append(node.left)
+
+                    if node.right:
+                        temp.append(node.right)
+                else:
+                    if node.right:
+                        temp.append(node.right)
+
+                    if node.left:
+                        temp.append(node.left)
+
+                if not q and nodes:
+                    res.append(nodes[:])
+                    nodes = []
+
+                if not q and temp:
+                    q = temp[:]
+                    temp = []
+                    level += 1
+
+        return res
+# @lc code=end
+
